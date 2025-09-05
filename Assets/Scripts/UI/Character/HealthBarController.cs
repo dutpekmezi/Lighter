ff8b4e37d81f8654236de0d195f796e50b36bcf1
Dut Pekmezi <sirakkaya@gmail.com>
2025-09-05 20:11:35 +0300

    ExpBar slider smoothing

--- Assets/Scripts/UI/Character/HealthBarController.cs
+++ Assets/Scripts/UI/Character/HealthBarController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace dutpekmezi
 {
@@ -8,12 +9,14 @@ namespace dutpekmezi
         [Header("References")]
         [SerializeField] private Slider slider;
 
+        [Header("Smooth Settings")]
+        [SerializeField] private float tweenDuration = 0.5f;
+
         private CharacterBase character;
 
         private void Start()
         {
             character = CharacterSystem.Instance.GetCharacter();
-
             Init();
         }
 
@@ -22,17 +25,29 @@ namespace dutpekmezi
             if (character != null)
             {
                 character.OnExpGained += UpdateSlider;
+
+                slider.minValue = 0;
+                slider.maxValue = CharacterSystem.Instance.GetExpForNextLevel();
+                slider.value = CharacterSystem.Instance.GetExp();
             }
         }
 
         private void UpdateSlider(CharacterBase character, int currentExp)
         {
+            DOTween.Kill(slider);
+
             if (slider != null)
             {
                 slider.minValue = 0;
                 slider.maxValue = CharacterSystem.Instance.GetExpForNextLevel();
 
-                slider.value = CharacterSystem.Instance.GetExp();
+                int targetExp = CharacterSystem.Instance.GetExp();
+
+                slider.DOValue(targetExp, tweenDuration).SetEase(Ease.OutCubic)
+                    .OnComplete(() =>
+                    {
+                        DOTween.Kill(slider);
+                    });
             }
         }
 
